2a82db100bb5ca052a48dcc01a607e89481e5e25
Ayush Agrawal <nitaandroid121@gmail.com>
2019-08-15 14:21:27 +0530

    Adding delete functionality in linklist

--- link_list.go
+++ link_list.go
@@ -1,22 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Node struct {
 	Data int64
 	Next *Node
 }
 
-type Linklisst struct {
+type Linklist struct {
 	Head *Node
 	Size int16
 }
 
-func (f *Linklisst) addNodeEnd(element int64) {
+func (f *Linklist) addNodeEnd(element int64) {
 
 	newnode := Node{element, nil}
 
 	if f.Head == nil {
+		f.Size++
 		f.Head = &newnode
 		return
 	}
@@ -28,11 +32,41 @@ func (f *Linklisst) addNodeEnd(element int64) {
 		prev = current
 		current = current.Next
 	}
+	f.Size++
 	prev.Next = &newnode
 	return
 }
 
-func (f Linklisst) printList() {
+func (f *Linklist) deleteElementAt(position int16) (bool, error) {
+
+	if position < 0 {
+		return false, errors.New(" Invalid Position, position must be a positive value")
+	}
+	if position >= f.Size {
+		return false, errors.New("Out of range")
+	}
+
+	if f.Head == nil {
+		return false, nil
+	}
+	if position == 0 {
+		f.Size--
+		f.Head = f.Head.Next
+		return true, nil
+	}
+	var prev, current *Node = nil, f.Head
+	i := int16(0)
+	for i < position {
+		prev = current
+		current = current.Next
+		i++
+	}
+	f.Size--
+	prev.Next = current.Next
+	return true, nil
+}
+
+func (f Linklist) printList() {
 	current := f.Head
 	for current != nil {
 		fmt.Println(current.Data)
@@ -41,9 +75,17 @@ func (f Linklisst) printList() {
 }
 
 func main() {
-	var ll Linklisst
+	var ll Linklist
 	ll.addNodeEnd(10)
 	ll.addNodeEnd(20)
 	ll.addNodeEnd(30)
+	ll.printList()
+
+	_, err := ll.deleteElementAt(0)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(ll.Size)
 	ll.printList()
 }
